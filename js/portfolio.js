4716f3876cec81f2f89f5c9b349c12160a37aa22
Meghan <meghan.louttit@nytimes.com>
2015-01-27 16:49:00 -0500

    added weekly files

---
+++ js/portfolio.js
@@ -0,0 +1,32 @@
+{
+	"portfolio":[
+		{
+			"title":"Out, and Serving",
+			"image":"http://placekitten.com/g/200/150",
+			"type":"photographs",
+			"url":"http://www.nytimes.com/interactive/2014/03/09/opinion/sunday/exposures-military.html",
+			"pubDate":"March 9, 2014"
+		},
+		{
+			"title":"State of the States",
+			"image":"http://placekitten.com/g/200/150",
+			"type":"graphic",
+			"url":"http://www.nytimes.com/interactive/2014/01/28/us/28-stateofstates.html",
+			"pubDate":"January 28, 2014"
+		},
+		{
+			"title":"Notes on Blindness",
+			"image":"http://placekitten.com/g/200/150",
+			"type":"video",
+			"url":"http://www.nytimes.com/interactive/2014/01/16/opinion/16OpDoc-NotesOnBlindness.html",
+			"pubDate":"January 16, 2014"
+		},
+		{
+			"title":"Test Project",
+			"image":"http://placekitten.com/g/200/150",
+			"type":"video",
+			"url":"http://www.nytimes.com/interactive/2014/01/16/opinion/16OpDoc-NotesOnBlindness.html",
+			"pubDate":"March 24, 2014"
+		}
+	]
+}
