e9dab436df66dfeba5f269f219c30a4392da62f8
Meghan <meghan.louttit@nytimes.com>
2015-03-23 12:32:20 -0400

    week 8 stuff

--- js/lessonHeader.js
+++ js/lessonHeader.js
@@ -1,8 +1,8 @@
 window.onload = function() {
 
-	var banner = '<p><a href="/cuny/">&laquo; Home</a> | <a href="../w1">Week 1</a> | <a href="../w2">Week 2</a> | <a href="../w3">Week 3</a> | <a href="../w4">Week 4</a> | <a href="../w5">Week 5</a> | <a href="../w6">Week 6</a> | <a href="../w7">Week 7</a> <!--| <a href="../w8">Week 8</a> | <a href="../w9">Week 9</a> | <a href="../w10">Week 10</a> | <a href="../w11">Week 11</a> | <a href="../w12">Week 12</a> | <a href="../w13">Week 13</a> | <a href="../w14">Week 14</a>--></p>';
+	var banner = '<p><a href="/cuny/">&laquo; Home</a> | <a href="../w1">Week 1</a> | <a href="../w2">Week 2</a> | <a href="../w3">Week 3</a> | <a href="../w4">Week 4</a> | <a href="../w5">Week 5</a> | <a href="../w6">Week 6</a> | <a href="../w7">Week 7</a> | <a href="../w8">Week 8</a> <!--| <a href="../w9">Week 9</a> | <a href="../w10">Week 10</a> | <a href="../w11">Week 11</a> | <a href="../w12">Week 12</a> | <a href="../w13">Week 13</a> | <a href="../w14">Week 14</a>--></p>';
 
-	var total = 8;
+	var total = 9;
 
 	var nav = document.getElementById("backTo");
 	nav.innerHTML = banner;
