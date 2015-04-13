d6b6ed01033c68428c01ecb558902be4f2f9ee7b
Meghan <meghan.louttit@nytimes.com>
2015-04-13 17:25:03 -0400

    week 10

--- lessons/w10/js/json.js
+++ lessons/w10/js/json.js
@@ -4,18 +4,21 @@
       "title":"Out, and Serving",
       "type":"photographs",
       "url":"http://www.nytimes.com/interactive/2014/03/09/opinion/sunday/exposures-military.html",
-      "pubDate":"March 9, 2014"
+      "pubDate":"March 9, 2014",
+      "image":"http://placekitten.com/g/200/133"
     },
     {
       "title":"State of the States",
       "type":"graphic",
       "url":"http://www.nytimes.com/interactive/2014/01/28/us/28-stateofstates.html",
-      "pubDate":"January 28, 2014"
+      "pubDate":"January 28, 2014",
+      "image":"http://placekitten.com/g/200/133"
     },
     {
       "title":"Notes on Blindness",
       "type":"video",
       "url":"http://www.nytimes.com/interactive/2014/01/16/opinion/16OpDoc-NotesOnBlindness.html",
-      "pubDate":"January 16, 2014"
+      "pubDate":"January 16, 2014",
+      "image":"http://placekitten.com/g/200/133"
     }
   ]
