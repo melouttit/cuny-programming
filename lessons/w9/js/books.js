5f2b579d28df4ea6eb6f16e07e0e5d13c7cf2f4a
Meghan <meghan.louttit@nytimes.com>
2016-04-03 19:20:02 -0400

    add books json

---
+++ lessons/w9/js/books.js
@@ -0,0 +1,27 @@
+"summary":"My Favorite Books",
+"books":[
+    {
+      "title":"The Thornbirds",
+      "genre":"fiction",
+      "author":"Collen McCullough",
+      "yearPubbed":"1977",
+      "image":"http://placekitten.com/g/200/133",
+      "buy":"http://www.amazon.com/The-Thorn-Birds-Colleen-McCullough/dp/0380018179"
+    },
+    {
+      "title":"The Shock Doctrine: The Rise of Disaster Capitalism",
+      "genre":"non-fiction",
+      "author":"Naomi Klein",
+      "yearPubbed":"2008",
+      "image":"http://placekitten.com/g/200/133",
+      "buy":"http://www.amazon.com/The-Shock-Doctrine-Disaster-Capitalism/dp/0312427999"
+    },
+    {
+      "title":"Night of the Gun",
+      "genre":"memoir",
+      "author":"David Carr",
+      "yearPubbed":"2008",
+      "image":"http://placekitten.com/g/200/133",
+      "buy":"http://www.amazon.com/Night-Gun-reporter-investigates-darkest-ebook/dp/B001DXNZ9Q/"
+    },
+  ]
